f85c0882812208a559c9a70d45c1a94e3257f6b8
Donya <thisis2nya@gmail.com>
2024-03-30 13:00:19 +0330

    fix: prevent from adding a task which already exists

--- src/stores/todo-list.ts
+++ src/stores/todo-list.ts
@@ -10,6 +10,13 @@ export const useTodoList = defineStore("todo-list", () => {
 
   // add a new task
   const addTask = (taskTitle: string): void => {
+    // check if there's a task with the same title
+    const identifiedTask = tasks.value.find((task) => task.title == taskTitle);
+    if (identifiedTask) {
+      message.error("You already have a task with the same title");
+      return;
+    }
+
     const task: TaskInfo = {
       title: taskTitle as string,
       isDone: false,
