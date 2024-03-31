a02b1c645505e6965b6f1066c3b0e17a21af9ea1
Donya <thisis2nya@gmail.com>
2024-04-01 00:11:29 +0330

    feat: complete profile settings page

---
+++ src/interfaces/profile.interface.ts
@@ -0,0 +1,5 @@
+export interface ProfileInfo {
+  name: string;
+  locale: "en" | "fa";
+  theme: "white-clouds" | "fresh-leaves" | "purple-moon" | "relaxing-cottage";
+}
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,9 +1,146 @@
 <template>
   <section
-    class="w-full md:h-full py-12 flex flex-col items-center justify-center gap-12">
-    <!-- welcome text -->
-    <h2 class="text-lg lg:text-2xl text-neutral-200 font-mono">
-      Welcome, Donya Davoodi :)
-    </h2>
+    class="w-[90vw] md:w-[700px] md:h-full mx-auto py-12 flex flex-col items-center justify-center gap-12">
+    <a-form
+      class="w-full flex flex-col items-start justify-start gap-8"
+      :model="profileData"
+      :label-col="{ span: 8 }"
+      :wrapper-col="{ span: 16 }"
+      autocomplete="off"
+      @finish="onFinish"
+      @finishFailed="onFinishFailed">
+      <a-flex align="center" justify="center" gap="24" class="w-full">
+        <!-- name -->
+        <a-space class="flex-1" direction="vertical">
+          <label class="!text-base text-white">Name</label>
+          <a-input
+            class="w-full"
+            v-model:value="profileData.name"
+            placeholder="Enter your name" />
+        </a-space>
+
+        <!-- locale -->
+        <a-space class="flex-1" direction="vertical">
+          <label class="!text-base text-white">Locale</label>
+          <a-select
+            ref="select"
+            class="w-full"
+            v-model:value="profileData.locale"
+            :options="selectOptions"></a-select>
+        </a-space>
+      </a-flex>
+
+      <!-- theme -->
+      <a-flex vertical align="start" justify="center" gap="10" class="w-full">
+        <label class="!text-base text-white">Theme Background</label>
+
+        <!-- backgrounds -->
+        <!-- <div class="w-full grid grid-cols-2 md:grid-cols-4 gap-5"> -->
+        <a-radio-group
+          v-model:value="profileData.theme"
+          class="w-full grid grid-cols-2 md:grid-cols-4 gap-5">
+          <!-- <a-radio-group v-model:value="profileData.theme"> -->
+          <!-- </a-radio-group> -->
+          <a-radio
+            :key="i"
+            v-for="(item, i) in backgrouds"
+            :value="item"
+            :class="item == profileData.theme ? 'border-2 border-white' : ''"
+            class="relative w-full h-44 md:h-20 flex items-center justify-center rounded-xl md:rounded-md shadow-xl hover:shadow-black/30 overflow-hidden transition-all duration-75 group">
+            <!-- overlay (name) -->
+            <div
+              class="absolute top-0 left-0 w-full h-full bg-neutral-900/60 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-all duration-300 z-20">
+              <span class="capitalize text-sm text-white">{{
+                item.split("-").join(" ")
+              }}</span>
+            </div>
+
+            <!-- overlay (checked) -->
+            <div
+              v-if="item == profileData.theme"
+              class="absolute top-0 left-0 w-full h-full bg-neutral-900/60 flex items-center justify-center z-10">
+              <check-circle-filled
+                class="text-3xl md:text-xl text-neutral-300"></check-circle-filled>
+            </div>
+
+            <!-- image -->
+            <img
+              class="min-w-full min-h-full"
+              :src="`/img/backgrounds/${item}.jpg`"
+              :alt="item.split('-').join(' ')" />
+          </a-radio>
+        </a-radio-group>
+        <!-- </div> -->
+      </a-flex>
+
+      <!-- save button -->
+      <a-button
+        type="primary"
+        class="h-auto px-6 py-2 text-base text-black bg-white"
+        >Save changes</a-button
+      >
+    </a-form>
   </section>
 </template>
+<script setup lang="ts">
+import { ref, reactive } from "vue";
+import { CheckCircleFilled } from "@ant-design/icons-vue";
+
+// interfaces
+import { ProfileInfo } from "../interfaces/profile.interface";
+
+// variables
+const profileData = reactive<ProfileInfo>({
+  name: "",
+  locale: "en",
+  theme: "white-clouds",
+});
+const selectOptions = ref<object[]>([
+  {
+    value: "en",
+    label: "en",
+  },
+  {
+    value: "fa",
+    label: "fa",
+  },
+]);
+const backgrouds = ref<string[]>([
+  "white-clouds",
+  "purple-moon",
+  "fresh-leaves",
+  "relaxing-cottage",
+]);
+
+// methods
+const onFinish = (values: any) => {
+  console.log("Success:", values);
+};
+const onFinishFailed = (errorInfo: any) => {
+  console.log("Failed:", errorInfo);
+};
+</script>
+
+<style>
+.ant-radio {
+  display: none !important;
+}
+.ant-radio-wrapper input:checked {
+  display: none !important;
+}
+.ant-radio-wrapper span {
+  padding: 0 !important;
+}
+.ant-input,
+.ant-input *,
+.ant-input:focus,
+.ant-input::placeholder,
+.ant-select-selection-item {
+  color: #000000 !important;
+}
+.ant-input,
+.ant-select-selector {
+  font-size: 16px !important;
+  background-color: #f7f7f770 !important;
+}
+</style>
